9c4a1c1ddbdbf351c0b9798d8091e6011a1b58cb
Smony <dev.symonchuk@gmail.com>
2019-09-17 15:09:35 +0300

    ajax ..

--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -3,29 +3,38 @@
 namespace Smony\Contact\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Smony\Contact\Http\Requests\ContactRequest;
 use Smony\Contact\Mail\ContactMailable;
 use Smony\Contact\Models\Contact;
 
+/**
+ * Class ContactController
+ * @package Smony\Contact\Http\Controllers
+ */
 class ContactController extends Controller
 {
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('contact::contact');
     }
 
     /**
-     * @param Request $request
-     * @return array
+     * @param ContactRequest $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function send(Request $request)
+    public function send(ContactRequest $request)
     {
+        $attributes = $request->validated();
 
-        Mail::to(config('contact.send_email_to'))->send(new ContactMailable($request->message, $request->name));
-        Contact::create($request->all());
-
-        return redirect(route('contact'));
+        if ($attributes) {
+            Mail::to(config('contact.send_email_to'))->send(new ContactMailable($request->message, $request->name));
+            Contact::create($attributes);
+            return response()->json(['success' => 'Added new records.']);
+        }
     }
 }
---
+++ src/Http/Requests/ContactRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Smony\Contact\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'message' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'A name is required',
+            'email.required'  => 'A message is required',
+            'subject.required'  => 'A subject is required',
+            'message.required'  => 'A message is required',
+        ];
+    }
+}
